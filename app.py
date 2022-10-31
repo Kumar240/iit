0cf48179c5a5861ee709f53fa476eca3697a568e
Ubuntu <ubuntu@ip-172-31-46-136.ap-south-1.compute.internal>
2022-10-31 06:40:19 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+if __name__ == "__main__":
+	app.run(host="172.31.46.136" , port=5000, debug=True)
